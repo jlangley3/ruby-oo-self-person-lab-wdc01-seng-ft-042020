5a28d3aaca2e94efe9eb5bbaf2b06a2e5abc6ad0
jlangley3 <jlangleyiii@aol.com>
2020-04-22 13:25:07 -0400

    just starting

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
-# your code goes here
+class Person
+    attr_writer :name
+    def initialize(name, bank_account = 25)
+        @name = name
+        @bank_account = bank_account
+    end
+    #def name
+        #@name
+    #end
+    def bank_account
+        @bank_account = bank_account
+     end
+    def bank_account=(new_money)
+       @bank_account = new_money
+    end
+    
+end# your code goes here
